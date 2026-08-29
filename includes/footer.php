a102e2c8375dbc08101445481e7ea7ed5a559a2f
Monarch-muerte <rudrakhamar009@gmail.com>
2026-08-29 17:33:54 +0530

    changes all the features for admin's request.

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-  </main>
-  <footer>College Attainment Portal &middot; Course Exit (CO1&ndash;CO6) &middot; Program/Activity Exit (PO1&ndash;PO11, PSO1&ndash;PSO2)</footer>
-</div>
-</body>
-</html>
